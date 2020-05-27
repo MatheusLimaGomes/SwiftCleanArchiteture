d66332ade3326223df14c534652641da7769c955
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-05-27 19:45:53 -0300

    add Infrastructure Layer and rename groups in other targets

---
+++ InfrastructureTests/AlamofireAdapterTestes.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireAdapterTestes .swift
+//  InfrastructureTests
+//
+//  Created by Matheus Lima Gomes on 27/05/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTestes: XCTestCase {
+    func test_() throws {
+        
+    }
+}
