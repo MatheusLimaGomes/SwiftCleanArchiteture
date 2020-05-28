d3188637d7d0ac2ee01529d39ff4c439a1950244
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-05-28 09:24:38 -0300

    code organization

---
+++ Data/Http/HttpErros/HttpClientError.swift
@@ -0,0 +1,13 @@
+//
+//  HttpErrors.swift
+//  Data
+//
+//  Created by Matheus Lima Gomes on 23/05/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import Foundation
+
+public enum HttpClientError: Error {
+    case noConnectivityError
+}
