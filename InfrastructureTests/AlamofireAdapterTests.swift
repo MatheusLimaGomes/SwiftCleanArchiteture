eb217bfae5c0aca4958c48f753e3943543ed522f
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-05-27 23:03:32 -0300

    add data extension to convert Json

--- InfrastructureTests/AlamofireAdapterTests.swift
+++ InfrastructureTests/AlamofireAdapterTests.swift
@@ -16,8 +16,8 @@ class AlamofireAdapter {
         self.session = session
     }
     func post(to url: URL, with data: Data?) {
-        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
+        session.request(url, method: .post, parameters: data?
+            .toJson(), encoding: JSONEncoding.default).resume()
     }
 }
 class AlamofireAdapterTests: XCTestCase {
