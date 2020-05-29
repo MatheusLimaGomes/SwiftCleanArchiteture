c7ac85572e5a48b028a2a78eabc1217a0eebc8fb
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-05-29 10:22:59 -0300

    move URLProtocolStub to own file

--- InfrastructureTests/AlamofireAdapterTests.swift
+++ InfrastructureTests/AlamofireAdapterTests.swift
@@ -123,39 +123,3 @@ extension AlamofireAdapterTests {
         wait(for: [exp], timeout: 1)
     }
 }
-class URLProtocolStub: URLProtocol {
-    static var emit: ((URLRequest) -> Void)?
-    static var data: Data?
-    static var response: HTTPURLResponse?
-    static var error: Error?
-    
-    static func setup(data: Data?, response: HTTPURLResponse?, error: Error?) {
-        self.data = data
-        self.response  = response
-        self.error = error
-    }
-    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
-        URLProtocolStub.emit = completion
-    }
-    override open class func canInit(with request: URLRequest) -> Bool {
-        return true
-    }
-    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-    override open func startLoading() {
-        URLProtocolStub.emit?(request)
-        if let data =  URLProtocolStub.data {
-            client?.urlProtocol(self, didLoad: data)
-        }
-        if let response = URLProtocolStub.response {
-            client?.urlProtocol(self, didReceive: response,
-                                cacheStoragePolicy: .notAllowed)
-        }
-        if let error = URLProtocolStub.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-        client?.urlProtocolDidFinishLoading(self)
-    }
-    override open func stopLoading() {}
-}
