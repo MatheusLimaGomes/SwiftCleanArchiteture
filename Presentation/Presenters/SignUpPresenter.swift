0853497f7d0be9896f19dd4ff210badc64cfb4f9
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-06-26 19:55:04 -0300

    move SignupViewModel to own viewModel

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -10,21 +10,6 @@ import Foundation
 import Domain
 
 
-public struct SignUpViewModel {
-    public var name: String?
-    public var email: String?
-    public var password: String?
-    public var passwordConfirmation: String?
-    
-    public init(name: String? = nil, email: String? = nil,
-                password: String? = nil, passwordConfirmation: String? = nil) {
-        self.name = name
-        self.email = email
-        self.password = password
-        self.passwordConfirmation = passwordConfirmation
-    }
-}
-
 public final class SignUpPresenter {
     private let alertView: AlertView
     private let emailValidator: EmailValidator
