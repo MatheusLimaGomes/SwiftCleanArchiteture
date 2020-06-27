7230e2208017ed9e0f7c1710c13f29b18b04fa22
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-06-26 23:38:24 -0300

    add View in UILayer

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Matheus Lima Gomes on 26/06/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import UIKit
+
+public final class SignUpViewController: UIViewController {
+
+    let loadingIndicator: UIActivityIndicatorView? = nil
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewControllerTests .swift
+//  UITests
+//
+//  Created by Matheus Lima Gomes on 26/06/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import XCTest
+import UIKit
+import UI
+
+class SignUpViewControllerTests: XCTestCase {
+    func test_loading_is_hidden_on_start() throws {
+      
+    }
+}
