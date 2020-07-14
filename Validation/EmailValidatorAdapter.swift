694f8fa5dec398a087fc6095a1a6efac27db7f3e
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-07-13 22:07:35 -0300

    move email validator adapter to own file

---
+++ Validation/EmailValidatorAdapter.swift
@@ -0,0 +1,20 @@
+//
+//  EmailValidatorAdapter.swift
+//  Validation
+//
+//  Created by Matheus Lima Gomes on 13/07/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}
