effd5a0779bb572955b16631d206406e056f2b7e
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-07-13 22:15:27 -0300

    add main layer and firs setup

---
+++ main/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  main
+//
+//  Created by Matheus Lima Gomes on 13/07/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
