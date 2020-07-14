23e0fbeaefd2480488d483e5479e350f9325d87c
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-07-13 21:54:02 -0300

    ensure Email Validator Adapter returns false if email is invalid

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -7,11 +7,33 @@
 //
 
 import XCTest
+import Presentation
 
 class EmailValidatorAdapterTests: XCTestCase {
-
-    func test_() throws {
-        
+    
+    func test_invalidEmails() throws {
+        let sut = EmailValidatorAdapter()
+        XCTAssertFalse(sut.isValid(email: "rr"))
+        XCTAssertFalse(sut.isValid(email: "rr@"))
+        XCTAssertFalse(sut.isValid(email: "rr@."))
+        XCTAssertFalse(sut.isValid(email: "rr@@"))
+        XCTAssertFalse(sut.isValid(email: "@rr.2"))
+        XCTAssertFalse(sut.isValid(email: "123"))
+        XCTAssertFalse(sut.isValid(email: "r@r.2"))
+        XCTAssertFalse(sut.isValid(email: "rr@w.c"))
+        XCTAssertFalse(sut.isValid(email: "rr@domain.c"))
     }
+    
+}
 
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+    
+    
 }
