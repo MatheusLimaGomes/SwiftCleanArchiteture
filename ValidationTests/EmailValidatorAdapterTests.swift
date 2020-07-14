49fc033e0341e9fc243d7516e745351a68498dec
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2020-07-13 21:40:17 -0300

    add email validation layer

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  ValidationTests
+//
+//  Created by Matheus Lima Gomes on 13/07/20.
+//  Copyright © 2020 Matheus Lima Gomes. All rights reserved.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() throws {
+        
+    }
+
+}
